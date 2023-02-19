902d8ffef309a5b3416190be95454127b42db095
scazan <scottcazan@gmail.com>
2023-02-19 20:38:09 +0100

    add Dockerfile and fly.toml for easy deployment to fly.io

--- src/main.rs
+++ src/main.rs
@@ -34,12 +34,12 @@ async fn process_client_messages(my_id: usize, sink : ClientSink, mut stream: Cl
             Message::Close(_) => break,
             _ => m,
         };
-        
+
         for (id, i) in all.borrow().iter() {
             if id == my_id { continue; }
-            
+
             let mut b = i.borrow_mut();
-            
+
             // Send if possible, throw away message if not ready
             let _ = b.send(fwd.clone()).now_or_never();
         }
@@ -49,16 +49,16 @@ async fn process_client_messages(my_id: usize, sink : ClientSink, mut stream: Cl
 }
 
 async fn serve_client(client: UsualClient, all:AllClients) -> Result<()> {
-    
+
     let (sink, stream) = client.split();
     let sink : ClientSink = Rc::new(RefCell::new(sink));
-    
-    
+
+
     let my_id : usize = all.borrow_mut().insert(sink.clone());
 
-    
+
     let ret = process_client_messages(my_id, sink, stream, &all).await;
-    
+
     let sink = all.borrow_mut().remove(my_id);
     sink.borrow_mut().send(Message::Close(None)).await?;
     ret?;
@@ -71,7 +71,7 @@ async fn client_accepting_loop(listener: tokio::net::TcpListener) -> Result<()> 
 
     let mapping : Rc<RefCell<Url2Clientset>> = Rc::new(RefCell::new(HashMap::new()));
     let num_urls : Rc<Cell<usize>> = Rc::new(Cell::new(0));
-    
+
     let mut config = tungstenite::protocol::WebSocketConfig::default();
 
     config.max_send_queue = Some(BUFMSG);
@@ -96,13 +96,13 @@ async fn client_accepting_loop(listener: tokio::net::TcpListener) -> Result<()> 
                 let clientset : AllClients;
                 {
                     let mut map = mapping.borrow_mut();
-                    
+
                     if !map.contains_key(&*url) {
                         if num_urls.get() >= MAXURLS {
                             println!("Rejected");
                             return;
                         }
-                    
+
                         let new_clientset : AllClients = Rc::new(RefCell::new(Slab::new()));
                         map.insert(url.clone(), new_clientset);
                         println!("New URL: {}", url);
@@ -133,7 +133,7 @@ async fn client_accepting_loop(listener: tokio::net::TcpListener) -> Result<()> 
                         num_urls.set(num_urls.get() - 1);
                     }
                 }
-                
+
                 println!("- {} -> {}", from_addr, url);
             } else {
                 println!("Failed WebSocket connection attempt from {}", from_addr);
@@ -145,19 +145,21 @@ async fn client_accepting_loop(listener: tokio::net::TcpListener) -> Result<()> 
 
 #[tokio::main(flavor="current_thread")]
 async fn main() -> Result<()> {
-    let listen_addr : String;
-    
-    {
-        let a = std::env::args().collect::<Vec<_>>();
-        if a.len() != 2 || a[1] == "--help" || a[1] == "-?" {
-            println!("Usage: wsbroad listen_ip:listen_port");
-            ::std::process::exit(1);
-        }
-        listen_addr = a[1].clone();
-    }
+    // just quickly disabling this for my use case and hardcoding the listening address to match
+    // let listen_addr : String;
+
+    // the Dockerfile
+    // {
+        // let a = std::env::args().collect::<Vec<_>>();
+        // if a.len() != 2 || a[1] == "--help" || a[1] == "-?" {
+            // println!("Usage: wsbroad listen_ip:listen_port");
+            // ::std::process::exit(1);
+        // }
+        // listen_addr = a[1].clone();
+    // }
 
 
-    let listener = tokio::net::TcpListener::bind(&listen_addr).await?;
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await?;
 
     let ls = tokio::task::LocalSet::new();
     ls.run_until(client_accepting_loop(listener)).await
